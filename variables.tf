f911f4efbc8c04dc21a10f2d85d8bea3a39cf133
karthikeyan-g16 <karthikeyangopal16@gmail.com>
2025-07-29 03:33:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,13 +2,13 @@ variable "tools" {
   default = {
 
     github-runner = {
-      instance_type = "t3.small"
+      instance_type = "t2.medium"
       policy_name = [
         "AdministratorAccess"
       ]
     }
     vault = {
-      instance_type = "t3.small"
+      instance_type = "t2.medium"
       policy_name = []
     }
 
